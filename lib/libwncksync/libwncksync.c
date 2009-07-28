759b377c57bb9613015204e612e3f3df1d5939f3
Jason Smith <jason@t500>
2009-07-27 21:37:33 -0400

    unique names
    (bzr r42)

--- lib/libwncksync/libwncksync.c
+++ lib/libwncksync/libwncksync.c
@@ -11,27 +11,32 @@
 #include "libwncksync.h"
 
 DBusGConnection *connection;
-GError *error;
-DBusGProxy *proxy;
+GError *wncksync_error;
+DBusGProxy *wncksync_proxy;
+gboolean wncksync_initialized = FALSE;
 
 void wncksync_init ()
 {
-	connection = dbus_g_bus_get (DBUS_BUS_SESSION, &error);
+	if (wncksync_initialized)
+		return;
+
+	wncksync_initialized = TRUE;
+	
+	connection = dbus_g_bus_get (DBUS_BUS_SESSION, &wncksync_error);
 	
 	if (connection == NULL) {
-		g_printerr ("Failed to open bus: %s\n", error->message);
-		g_error_free (error);	
+		g_printerr ("Failed to open bus: %s\n", wncksync_error->message);
+		g_error_free (wncksync_error);	
 	}
 	
-	proxy = dbus_g_proxy_new_for_name (connection, 
+	wncksync_proxy = dbus_g_proxy_new_for_name (connection, 
 					   "org.wncksync.Matcher", 
 					   "/org/wncksync/Matcher", 
 					   "org.wncksync.Matcher");
 										
-	if (!proxy) {
+	if (!wncksync_proxy) {
 		g_printerr ("Failed to get name owner.\n");	
 	}
-	
 }
 
 void wncksync_quit ()
@@ -41,32 +46,32 @@ void wncksync_quit ()
 
 void wncksync_register_desktop_file_for_pid (gchar *desktop_file, gint pid)
 {
-	error = NULL;
-	if (!dbus_g_proxy_call (proxy, 
+	if (!wncksync_initialized) wncksync_init ();
+
+	g_return_if_fail (desktop_file);
+	dbus_g_proxy_call_no_reply (wncksync_proxy, 
 	                        "RegisterDesktopFileForPid", 
-	                         &error, 
 	                         G_TYPE_STRING, desktop_file, 
 	                         G_TYPE_INT, pid, 
-	                         G_TYPE_INVALID, G_TYPE_INVALID)) {
-		g_printerr ("%s\n", error->message);
-		g_error_free (error);	                         
-	}
+	                         G_TYPE_INVALID);
 }
 
 GArray * wncksync_windows_for_desktop_file (gchar *desktop_file)
 {
+	if (!wncksync_initialized) wncksync_init ();
+
 	GArray *windows = g_array_new (FALSE, TRUE, sizeof (WnckWindow*));
 	GArray *arr;
-	error = NULL;
-	if (!dbus_g_proxy_call (proxy, 
+	wncksync_error = NULL;
+	if (!dbus_g_proxy_call (wncksync_proxy, 
 	                        "XidsForDesktopFile", 
-	                        &error, 
+	                        &wncksync_error, 
 	                        G_TYPE_STRING, desktop_file, 
 	                        G_TYPE_INVALID, 
 	                        DBUS_TYPE_G_UINT64_ARRAY, &arr, 
 	                        G_TYPE_INVALID)) {
-		g_printerr ("Failed to fetch xid array: %s\n", error->message);
-		g_error_free (error);
+		g_printerr ("Failed to fetch xid array: %s\n", wncksync_error->message);
+		g_error_free (wncksync_error);
 		//Error Handling
 		return windows;
 	}
@@ -83,22 +88,24 @@ GArray * wncksync_windows_for_desktop_file (gchar *desktop_file)
 
 gchar * wncksync_desktop_item_for_window (WnckWindow *window)
 {
+	if (!wncksync_initialized) wncksync_init ();
+
 	if (window == NULL)
 		return "";
 		
 	gulong xid = wnck_window_get_xid (window);
 
 	gchar *desktop_file;
-	error = NULL;
-	if (!dbus_g_proxy_call (proxy, 
+	wncksync_error = NULL;
+	if (!dbus_g_proxy_call (wncksync_proxy, 
 	                        "DesktopFileForXid", 
-	                        &error, 
+	                        &wncksync_error, 
 	                        G_TYPE_UINT64, xid, 
 	                        G_TYPE_INVALID, 
 	                        G_TYPE_STRING, &desktop_file, 
 	                        G_TYPE_INVALID)) {
-		g_printerr ("Failed to fetch desktop file: %s\n", error->message);
-		g_error_free (error);
+		g_printerr ("Failed to fetch desktop file: %s\n", wncksync_error->message);
+		g_error_free (wncksync_error);
 		
 		return "";
 	}
