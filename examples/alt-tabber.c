fc18265d3bcbb983e73177a03fa2f3d630b67262
Neil Jagdish Patel <neil.patel@canonical.com>
2010-01-18 10:45:31 +0000

    [release] 0.2.2 -- more licensing fixes
    (bzr r88)

--- examples/alt-tabber.c
+++ examples/alt-tabber.c
@@ -4,8 +4,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; either version 3 of the License
  *
  * <program name> is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,10 +13,10 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301  USA
  */
- 
+
 #define WNCK_I_KNOW_THIS_IS_UNSTABLE
 
 #include <stdio.h>
@@ -35,9 +34,9 @@ GArray * get_desktop_files ()
 {
 	WnckScreen *screen = wnck_screen_get_default ();
 	GList *windows = wnck_screen_get_windows (screen);
-	
+
 	GArray *desktopFiles = g_array_new (FALSE, TRUE, sizeof (GString*));
-	
+
 	GList *window;
 	for (window = windows; window != NULL; window = window->next) {
 		/* Doing just a little bit of tracking right here can save a lot of dbus calls.
@@ -47,7 +46,7 @@ GArray * get_desktop_files ()
 		gchar *file = wncksync_desktop_item_for_xid (wnck_window_get_xid (window->data));
 		if (file != NULL && *file != 0) {
 			GString *desktopFile = g_string_new (file);
-						
+
 			gboolean equal = FALSE;
 			int i;
 			for (i = 0; i < desktopFiles->len; i++) {
@@ -56,31 +55,31 @@ GArray * get_desktop_files ()
 					break;
 				}
 			}
-			
+
 			if (!equal)
 				g_array_append_val (desktopFiles, desktopFile);
 		}
-		
+
 		g_free (file);
 	}
-	
+
 	return desktopFiles;
 }
 
 void populate_tree_store (GtkTreeStore *store)
 {
 	GtkTreeIter position, child;
-	
+
 	GArray *desktopFiles = get_desktop_files ();
-	
+
 	int i;
 	for (i = 0; i < desktopFiles->len; i++) {
 		gtk_tree_store_append (store, &position, NULL);
-		
+
 		gchar *string = g_array_index (desktopFiles, GString*, i)->str;
-		
+
 		gtk_tree_store_set (store, &position, 0, string, -1);
-		
+
 		GArray * windows = wncksync_xids_for_desktop_file (string);
 		int j;
 		for (j = 0; j < windows->len; j++) {
@@ -101,19 +100,19 @@ GtkWidget * make_tree_view ()
 
 	treeView = gtk_tree_view_new ();
 	treeStore = gtk_tree_store_new (1, G_TYPE_STRING);
-	
+
 	column = gtk_tree_view_column_new ();
 	gtk_tree_view_column_set_title (column, "windows");
 	gtk_tree_view_append_column (GTK_TREE_VIEW (treeView), column);
-	
+
 	renderer = gtk_cell_renderer_text_new ();
 	gtk_tree_view_column_pack_start (column, renderer, TRUE);
 	gtk_tree_view_column_add_attribute(column, renderer, "text", 0);
-	
+
 	gtk_tree_view_set_model (GTK_TREE_VIEW (treeView), GTK_TREE_MODEL (treeStore));
-	
+
 	g_object_unref (treeStore);
-	
+
 	return treeView;
 }
 
@@ -123,9 +122,9 @@ static gboolean on_timer_elapsed (gpointer callback_data)
 
 	gtk_tree_store_clear (store);
 	populate_tree_store (store);
-	
+
 	timer = 0;
-	
+
 	return FALSE;
 }
 
@@ -138,7 +137,7 @@ static void handle_window_opened_closed (WnckScreen *screen, WnckWindow *window,
 {
 	if (timer > 0)
 		g_source_remove (timer);
-	
+
 	timer = g_timeout_add (300, (GSourceFunc) on_timer_elapsed, NULL);
 }
 
@@ -146,10 +145,10 @@ static void destroy (GtkWidget *widget, gpointer data)
 {
 	gtk_main_quit ();
 }
-	
+
 /* This entire program is intentionally implemented in a slightly niave manner to allow it to show
  * the function of both of the primary calls to libwncksync. There is usually no reason to use
- * both as one or the other can generally get the job done. Experienced coders should recognize how 
+ * both as one or the other can generally get the job done. Experienced coders should recognize how
  * to do this right away, however it is noted where it could be performed in the source.
  */
 int main (int argc, char **argv)
@@ -157,23 +156,23 @@ int main (int argc, char **argv)
 	gtk_init (&argc, &argv);
 
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	
+
 	g_signal_connect (G_OBJECT (window), "destroy", G_CALLBACK (destroy), NULL);
 
 	treeView = make_tree_view ();
 	gtk_container_add (GTK_CONTAINER (window), treeView);
-	
+
 	WnckScreen *screen = wnck_screen_get_default ();
-	
+
 	g_signal_connect (G_OBJECT (screen), "window-opened", (GCallback) handle_window_opened_closed, window);
 	g_signal_connect (G_OBJECT (screen), "window-closed", (GCallback) handle_window_opened_closed, window);
 
 	wncksync_init ();
-	
+
 	gtk_widget_show_all (window);
 	gtk_main ();
 
-	wncksync_quit ();	
+	wncksync_quit ();
 	return 0;
 }
 
--- examples/desktop-file-launcher.c
+++ examples/desktop-file-launcher.c
@@ -4,8 +4,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; either version 3 of the License.
  *
  * <program name> is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,10 +13,10 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301  USA
  */
- 
+
 #include <stdio.h>
 #include <gtk/gtk.h>
 #include <glib.h>
@@ -28,12 +27,12 @@ int main (int argc, char **argv)
 	gtk_init (&argc, &argv);
 
 	GAppInfo *appInfo = G_APP_INFO (g_desktop_app_info_new_from_filename (argv [1]));
-	
+
 	GError *error;
 	g_app_info_launch (appInfo, NULL, NULL, &error);
-	
+
 	g_print ("End");
-	
+
 	return 0;
 }
 
