bd1870bf8602ac4b88c74dbbfa11b5e6516bcfe1
Jason Smith <jason.smith@canonical.com>
2010-04-28 00:23:29 -0400

    forgot to add bamf.h [as fortold by the prophetic email]
    (bzr r127)

---
+++ src/bamf.h
@@ -0,0 +1,10 @@
+#ifndef __BAMF_H__
+#define __BAMF_H__
+
+#define BAMF_DBUS_PATH "/org/ayatana/bamf"
+#define BAMF_DBUS_SERVICE "org.ayatana.bamf"
+
+#define BAMF_CONTROL_PATH "/org/ayatana/bamf/control"
+#define BAMF_MATCHER_PATH "/org/ayatana/bamf/matcher"
+
+#endif
