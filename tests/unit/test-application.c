18de60c4deb9ff8e4e83f11116c123b6b944ad77
Jason Smith <jason.smith@canonical.com>
2010-05-05 06:29:27 -0400

    More application unit tests
    (bzr r152)

--- tests/unit/test-application.c
+++ tests/unit/test-application.c
@@ -21,7 +21,12 @@
 #include <stdlib.h>
 #include "bamf-application.h"
 
+#define DESKTOP_FILE "usr/share/applications/gnome-terminal.desktop"
+
 static void test_allocation          (void);
+static void test_desktop_file        (void);
+static void test_urgent              (void);
+static void test_urgent_event        (void);
 
 void
 test_application_create_suite (void)
@@ -29,6 +34,9 @@ test_application_create_suite (void)
 #define DOMAIN "/Application"
 
   g_test_add_func (DOMAIN"/Allocation", test_allocation);
+  g_test_add_func (DOMAIN"/DesktopFile", test_desktop_file);
+  g_test_add_func (DOMAIN"/Urgent", test_urgent);
+  g_test_add_func (DOMAIN"/Urgent/ChangedEvent", test_urgent_event);
 }
 
 static void
@@ -43,8 +51,85 @@ test_allocation (void)
   g_object_unref (application);
   g_assert (!BAMF_IS_APPLICATION (application));
 
-  application = bamf_application_new_from_desktop_file ("/usr/share/applications/gnome-terminal.desktop");
+  application = bamf_application_new_from_desktop_file (DESKTOP_FILE);
   g_assert (BAMF_IS_APPLICATION (application));
+
+  g_object_unref (application);
+  g_assert (!BAMF_IS_APPLICATION (application));
+}
+
+static void
+test_desktop_file (void)
+{
+  BamfApplication    *application;
+
+  /* Check it allocates */
+  application = bamf_application_new ();
+  g_assert (bamf_application_get_desktop_file (application) == NULL);
+
+  bamf_application_set_desktop_file (application, DESKTOP_FILE);
+  g_assert (g_strcmp0 (bamf_application_get_desktop_file (application), DESKTOP_FILE) == 0);
+
+  g_object_unref (application);
+
+  application = bamf_application_new_from_desktop_file (DESKTOP_FILE);
+  g_assert (g_strcmp0 (bamf_application_get_desktop_file (application), DESKTOP_FILE) == 0);
+
+  g_object_unref (application);
+}
+
+static void
+test_urgent (void)
+{
+  BamfApplication *application;
+
+  application = g_object_new (BAMF_TYPE_APPLICATION, NULL);
+  g_assert (!bamf_application_is_urgent (application));
+
+  bamf_application_set_urgent (application, TRUE);
+  g_assert (bamf_application_is_urgent (application));
+
+  bamf_application_set_urgent (application, FALSE);
+  g_assert (!bamf_application_is_urgent (application));
+
+  g_object_unref (application);
+  g_assert (!BAMF_IS_APPLICATION (application));
+}
+
+static gboolean urgent_event_fired;
+static gboolean urgent_event_result;
+
+static void
+on_urgent_event (BamfApplication *application, gboolean urgent, gpointer pointer)
+{
+  urgent_event_fired = TRUE;
+  urgent_event_result = urgent;
+}
+
+static void
+test_urgent_event (void)
+{
+  BamfApplication *application;
+
+  application = g_object_new (BAMF_TYPE_APPLICATION, NULL);
+  g_assert (!bamf_application_is_urgent (application));
+
+  g_signal_connect (G_OBJECT (application), "urgent-changed",
+		    (GCallback) on_urgent_event, NULL);
+
+  urgent_event_fired = FALSE;
+  bamf_application_set_urgent (application, TRUE);
+  g_assert (bamf_application_is_urgent (application));
+
+  g_assert (urgent_event_fired);
+  g_assert (urgent_event_result);
+
+  urgent_event_fired = FALSE;
+  bamf_application_set_urgent (application, FALSE);
+  g_assert (!bamf_application_is_urgent (application));
+
+  g_assert (urgent_event_fired);
+  g_assert (!urgent_event_result);
 
   g_object_unref (application);
   g_assert (!BAMF_IS_APPLICATION (application));
