0ff73b827c32f1bf8ef420e6f1ac3e12f84b6c7f
jassmith@gmail.com <>
2010-07-09 04:30:26 -0400

    Add parents API to bus for debugging purposes
    (bzr r231.7.27)

--- src/bamf-view.h
+++ src/bamf-view.h
@@ -76,6 +76,8 @@ char    ** bamf_view_get_children_paths (BamfView *view);
 
 GList    * bamf_view_get_children       (BamfView *view);
 
+char    ** bamf_view_get_parent_paths (BamfView *view);
+
 GList    * bamf_view_get_parents        (BamfView *view);
 
 char     * bamf_view_get_path           (BamfView *view);
