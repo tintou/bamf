9b1583df122bf532a2d9c49d454b2754e1688ec9
Jason Smith <jason.smith@canonical.com>
2011-08-01 19:25:01 -0400

    fix build errors with new toolchain
    (bzr r231.28.4)

--- src/main.c
+++ src/main.c
@@ -95,6 +95,8 @@ main (int argc, char **argv)
   control  = bamf_control_get_default ();
   approver = bamf_indicator_source_get_default ();
 
+  g_print ("They make me do something with things I dont need to do stuff with %p %p %p", matcher, control, approver);
+
   if (state_file)
     {
       bamf_legacy_screen_set_state_file (bamf_legacy_screen_get_default (), state_file);
