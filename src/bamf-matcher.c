a9c8d741af437226415b076807ee04597cf7238b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2011-12-10 22:04:19 +0100

    BamfMatcher: Adding myself to the authors.
    (bzr r231.36.10)

--- src/bamf-matcher.c
+++ src/bamf-matcher.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by: Jason Smith <jason.smith@canonical.com>
+ *              Marco Trevisan (Treviño) <3v1n0@ubuntu.com>
  *
  */
 
