99c714ae463c484b0a697a65373e5daade19b7b6
Marco Trevisan (Treviño) <mail@3v1n0.net>
2011-12-23 12:27:28 +0100

    Tests: updating copyright.
    (bzr r424.1.9)

--- tests/bamfdaemon/test-bamf.c
+++ tests/bamfdaemon/test-bamf.c
@@ -1,28 +1,25 @@
-//  
-//  Copyright (C) 2009 Canonical Ltd.
-// 
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-// 
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-// 
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+/*
+ * Copyright (C) 2011 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as 
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by Marco Trevisan (Treviño) <3v1n0@ubuntu.com>
+ *
+ */
 
 #include <stdio.h>
 #include <gtk/gtk.h>
 #include <gdk/gdk.h>
-/*
-#include <dbus/dbus.h>
-#include <dbus/dbus-glib.h>
-#include <dbus/dbus-glib-lowlevel.h>
-*/
 #include <glib.h>
 #include <gio/gio.h>
 #include <sys/types.h>
