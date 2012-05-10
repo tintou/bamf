1c4d948ed9cfc7a6bc6beabeb4e8cb0ddf270564
Robert Carr <racarr@canonical.com>
2012-05-10 16:15:42 -0700

    Add tab type to factory construction switch
    (bzr r464)

--- lib/libbamf/bamf-factory.c
+++ lib/libbamf/bamf-factory.c
@@ -184,6 +184,10 @@ BamfFactoryViewType compute_factory_type_by_str (const char *type)
         {
           factory_type = BAMF_FACTORY_VIEW;
         }
+      else if (g_strcmp0 (type, "tab") == 0)
+	{
+	  factory_type = BAMF_FACTORY_TAB;
+	}
     }
 
   return factory_type;
