ebf64280df583b950f0ae157bc946d5960e93abf
Marco Trevisan (Treviño) <mail@3v1n0.net>
2012-06-13 16:32:42 +0200

    BamfView: add a private function to set a view as closed
    (bzr r467.3.1)

--- lib/libbamf/bamf-view-private.h
+++ lib/libbamf/bamf-view-private.h
@@ -36,4 +36,7 @@ void _bamf_view_reset_flags (BamfView *view);
 void _bamf_view_set_name (BamfView *view, const char *name);
 
 void _bamf_view_set_icon (BamfView *view, const char *icon);
+
+void _bamf_view_set_closed (BamfView *view, gboolean closed);
+
 #endif
