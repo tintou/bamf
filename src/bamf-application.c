5702d856f22b849438ecce3c4c5dbcf57c503b26
Robert Carr <racarr@canonical.com>
2012-09-14 12:04:14 -0700

    Fix typo in bamf_unity_webapps_application_get_close_when_empty closing #1051042
    Fixes LP: #1051042
    
    (bzr r482.1.1)

--- src/bamf-application.c
+++ src/bamf-application.c
@@ -576,7 +576,7 @@ bamf_application_ensure_flags (BamfApplication *self)
 
   close_when_empty = bamf_application_get_close_when_empty (self);
   bamf_view_set_urgent       (BAMF_VIEW (self), urgent);
-  if (!((close_when_empty == FALSE) && running == FALSE))
+  if (close_when_empty == TRUE || running == TRUE)
     bamf_view_set_user_visible (BAMF_VIEW (self), visible);
   if ((running == TRUE) || close_when_empty)
     bamf_view_set_running      (BAMF_VIEW (self), running);
