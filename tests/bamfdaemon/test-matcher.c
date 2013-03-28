d79a426b112e3ff9f831e9100db361d00e1ae943
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-03-28 18:58:57 +0100

    TestMatcher: add tests to check the order of no-display .desktop files
    (bzr r524.3.7)

--- tests/bamfdaemon/test-matcher.c
+++ tests/bamfdaemon/test-matcher.c
@@ -28,7 +28,8 @@
 
 static void test_allocation (void);
 static void test_load_desktop_file (void);
-static void test_load_no_display_desktop_file_has_lower_prio (void);
+static void test_load_desktop_file_no_display_has_lower_prio_same_id (void);
+static void test_load_desktop_file_no_display_has_lower_prio_different_id (void);
 static void test_open_windows (void);
 static void test_match_desktopless_application (void);
 static void test_match_desktop_application (void);
@@ -54,7 +55,8 @@ test_matcher_create_suite (GDBusConnection *connection)
 
   g_test_add_func (DOMAIN"/Allocation", test_allocation);
   g_test_add_func (DOMAIN"/LoadDesktopFile", test_load_desktop_file);
-  g_test_add_func (DOMAIN"/LoadDesktopFile/NoDisplay", test_load_no_display_desktop_file_has_lower_prio);
+  g_test_add_func (DOMAIN"/LoadDesktopFile/NoDisplay/SameID", test_load_desktop_file_no_display_has_lower_prio_same_id);
+  g_test_add_func (DOMAIN"/LoadDesktopFile/NoDisplay/DifferentID", test_load_desktop_file_no_display_has_lower_prio_different_id);
   g_test_add_func (DOMAIN"/OpenWindows", test_open_windows);
   g_test_add_func (DOMAIN"/Matching/Application/DesktopLess", test_match_desktopless_application);
   g_test_add_func (DOMAIN"/Matching/Application/Desktop", test_match_desktop_application);
@@ -190,7 +192,7 @@ test_load_desktop_file (void)
 }
 
 static void
-test_load_no_display_desktop_file_has_lower_prio (void)
+test_load_desktop_file_no_display_has_lower_prio_same_id (void)
 {
   BamfMatcher *matcher = bamf_matcher_get_default ();
   BamfMatcherPrivate *priv = matcher->priv;
@@ -205,6 +207,31 @@ test_load_no_display_desktop_file_has_lower_prio (void)
 
   g_assert (l->next);
   g_assert_cmpstr (l->next->data, ==, DATA_DIR"/no-display/test-bamf-app.desktop");
+
+  l = g_hash_table_lookup (priv->desktop_id_table, "test-bamf-app");
+  g_assert (l);
+  g_assert_cmpstr (l->data, ==, TEST_BAMF_APP_DESKTOP);
+
+  g_assert (l->next);
+  g_assert_cmpstr (l->next->data, ==, DATA_DIR"/no-display/test-bamf-app.desktop");
+}
+
+static void
+test_load_desktop_file_no_display_has_lower_prio_different_id (void)
+{
+  BamfMatcher *matcher = bamf_matcher_get_default ();
+  BamfMatcherPrivate *priv = matcher->priv;
+
+  cleanup_matcher_tables (matcher);
+  bamf_matcher_load_desktop_file (matcher, DATA_DIR"/test-bamf-app-no-display.desktop");
+  bamf_matcher_load_desktop_file (matcher, DATA_DIR"/test-bamf-app-display.desktop");
+
+  GList *l = g_hash_table_lookup (priv->desktop_file_table, "test-bamf-app");
+  g_assert (l);
+  g_assert_cmpstr (l->data, ==, DATA_DIR"/test-bamf-app-display.desktop");
+
+  g_assert (l->next);
+  g_assert_cmpstr (l->next->data, ==, DATA_DIR"/test-bamf-app-no-display.desktop");
 }
 
 static void
