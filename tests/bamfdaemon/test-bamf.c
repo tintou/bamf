f78feadf88842e2c261fe6ffe171df3beea321d8
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-03-28 22:16:29 +0100

    TestBamf: set PATH only after that we got the bus
    (bzr r524.1.5)

--- tests/bamfdaemon/test-bamf.c
+++ tests/bamfdaemon/test-bamf.c
@@ -43,6 +43,8 @@ on_bus_acquired (GDBusConnection *connection, const gchar *name, gpointer data)
   test_view_create_suite (connection);
   test_window_create_suite ();
   test_application_create_suite (connection);
+
+  g_setenv("PATH", TESTDIR"/bamfdaemon/data/bin", TRUE);
   result = g_test_run ();
 
   g_main_loop_quit (loop);
@@ -63,8 +65,6 @@ main (gint argc, gchar *argv[])
   gtk_init (&argc, &argv);
   g_test_init (&argc, &argv, NULL);
   glibtop_init ();
-
-  g_setenv("PATH", TESTDIR"/bamfdaemon/data/bin", TRUE);
   g_setenv("BAMF_TEST_MODE", "TRUE", TRUE);
 
   loop = g_main_loop_new (NULL, FALSE);
