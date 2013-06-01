00031ae7aab398db86fcc7ad1e0fc1e5f63f4f03
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-06-01 19:08:09 +0200

    LibbamfTest: check for singleton, add some cleanup
    (bzr r424.8.71)

--- tests/libbamf/test-matcher.c
+++ tests/libbamf/test-matcher.c
@@ -1,33 +1,23 @@
-//  
+//
 //  Copyright (C) 2009 Canonical Ltd.
-// 
+//
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 
 #include <glib.h>
 #include <stdlib.h>
 #include "libbamf.h"
-
-static void test_allocation    (void);
-
-void 
-test_matcher_create_suite (void)
-{
-#define DOMAIN "/Matcher"
-
-  g_test_add_func (DOMAIN"/Allocation", test_allocation);  
-}
 
 static void
 test_allocation (void)
@@ -35,6 +25,28 @@ test_allocation (void)
   BamfMatcher *matcher;
 
   matcher = bamf_matcher_get_default ();
-
   g_assert (BAMF_IS_MATCHER (matcher));
+
+  g_object_unref (matcher);
+}
+
+static void
+test_singleton (void)
+{
+  BamfMatcher *matcher;
+
+  matcher = bamf_matcher_get_default ();
+  g_assert (BAMF_IS_MATCHER (matcher));
+  g_assert (matcher == bamf_matcher_get_default ());
+
+  g_object_unref (matcher);
+}
+
+void
+test_matcher_create_suite (void)
+{
+#define DOMAIN "/Matcher"
+
+  g_test_add_func (DOMAIN"/Allocation", test_allocation);
+  g_test_add_func (DOMAIN"/Singleton", test_singleton);
 }
