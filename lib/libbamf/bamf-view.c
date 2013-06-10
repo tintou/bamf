419e50bbffa28e3081a3aa18c82f67c71c0719c3
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-06-10 20:14:35 +0200

    BamfView: notify when the path of a view changes
    (bzr r424.9.11)

--- lib/libbamf/bamf-view.c
+++ lib/libbamf/bamf-view.c
@@ -765,6 +765,7 @@ _bamf_view_set_path (BamfView *view, const char *path)
 
   g_dbus_proxy_set_default_timeout (G_DBUS_PROXY (priv->proxy), BAMF_DBUS_DEFAULT_TIMEOUT);
   _bamf_view_reset_flags (view);
+  g_object_notify (G_OBJECT (self), "path");
 
   g_signal_connect (priv->proxy, "notify::active",
                     G_CALLBACK (bamf_view_on_active_changed), view);
