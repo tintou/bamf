c4674c868a65f40d9db73b6746665f9c8470791e
Marco Trevisan (Treviño) <mail@3v1n0.net>
2013-10-10 16:52:27 +0200

    BamfView: reload the view children everytime it changes dbus path
    Without doing it we could have children-less views in case we have just
    re-matched a view with a different dbus path (this happens in general
    for desktop-less applications), because the proxy won't be fast enough
    to catch the signal to update the inner children list.
    
    Fixes LP: #1238064
    
    (bzr r585.1.1)

--- lib/libbamf/bamf-view.c
+++ lib/libbamf/bamf-view.c
@@ -775,8 +775,11 @@ _bamf_view_set_path (BamfView *view, const char *path)
       return;
     }
 
-  priv = view->priv;
   bamf_view_unset_proxy (view);
+
+  priv = view->priv;
+  priv->reload_children = TRUE;
+
   priv->proxy = _bamf_dbus_item_view_proxy_new_for_bus_sync (G_BUS_TYPE_SESSION,
                                                              G_DBUS_PROXY_FLAGS_NONE,
                                                              BAMF_DBUS_SERVICE_NAME,
