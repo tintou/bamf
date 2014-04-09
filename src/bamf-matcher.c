a2e942b0ef7efe8558eebf5853497217c0dc44b9
Iain Lane <iain@orangesquash.org.uk>
2014-04-09 17:59:37 +0100

    Fix preifx regex (used for converting /usr/bin/python[stuff] foo into foo) to work with pythonX as well as python and pythonX.Y
    (bzr r599.1.1)

--- src/bamf-matcher.c
+++ src/bamf-matcher.c
@@ -55,7 +55,7 @@ static guint matcher_signals[LAST_SIGNAL] = { 0 };
 const gchar* EXEC_BAD_PREFIXES[] =
 {
   "^gksu(do)?$", "^sudo$", "^su-to-root$", "^amdxdg-su$", "^java(ws)?$", "^cli$",
-  "^mono$", "^ruby$", "^padsp$", "^aoss$", "^python(\\d.\\d)?$", "^(ba)?sh$",
+  "^mono$", "^ruby$", "^padsp$", "^aoss$", "^python(\\d(\\.\\d)?)?$", "^(ba)?sh$",
   "^perl$", "^env$", "^xdg-open$", "^qmlscene$", "^qmlviewer$",
   "^unity-webapps-runner$", "^webapp-container$",
   /* javaws strings: */ "^net\\.sourceforge\\.jnlp\\.runtime\\.Boot$", "^rt\\.jar$",
